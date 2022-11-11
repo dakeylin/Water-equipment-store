8b9f6fa89846a65a46b0d9c63791a206807ae2a3
dakeylin <dakeylin@gmail.com>
2022-11-11 18:00:10 +0300

    version 1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="/images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/images/arrow-right.svg" alt=""></button>',
+    });
+
+    $('.tab').on('click', function (event) {
+        event.preventDefault();
+
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    })
+
+    $('.product-item__favourite').on('click', function(){
+        $($(this)).toggleClass('product-item__favourite--active');
+    });
+
+    $('.product-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="/images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="/images/arrow-while-right.svg" alt=""></button>',
+    })
+});
